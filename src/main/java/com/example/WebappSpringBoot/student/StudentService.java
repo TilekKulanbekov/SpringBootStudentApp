6a40b86171d8802be9a9ec3872039ff8b484ba0d
Tilek <tilek.kulanbekov@iaau.edu.kg>
2022-04-10 15:51:51 +0600

    'lol'

--- src/main/java/com/example/WebappSpringBoot/student/StudentService.java
+++ src/main/java/com/example/WebappSpringBoot/student/StudentService.java
@@ -1,10 +1,14 @@
 package com.example.WebappSpringBoot.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
+
     public List<Student> getStudent() {
         return List.of(
                 new Student(
