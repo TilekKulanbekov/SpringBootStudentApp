de6375dd8521bd61c641f648fa1cfd0742990c0f
Tilek <tilek.kulanbekov@iaau.edu.kg>
2022-04-10 14:50:59 +0600

    first commit

---
+++ src/test/java/com/example/WebappSpringBoot/WebAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebappSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
